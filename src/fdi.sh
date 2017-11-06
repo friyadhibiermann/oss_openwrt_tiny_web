2d7962fed070266f8198733912e0792babad112a
friyadhibiermann <friyadhibiermann@gmail.com>
2017-11-06 18:12:24 +0700

    Update fdi.sh

--- src/fdi.sh
+++ src/fdi.sh
@@ -27,8 +27,8 @@ function waitForHost
 {
     if [ -n "$1" ];
     then
-        waitForHost1 $1 > /dev/null 2>&1
-                echo "Internet Connected"
+        waitForHost1 $1 
+                printf "\nInternet Connected\n"
     else
         echo -n "..."
     fi
@@ -39,10 +39,14 @@ function waitForHost1
     reachable=0;
     while [ $reachable -eq 0 ];
     do
-    $PING -q -c 1 $1
+    $PING -q -c 1 $1 > /dev/null 2>&1
     if [ "$?" -eq 0 ];
     then
         reachable=1
+	else
+		IP=$(ifconfig | awk '/wlan0/,/inet/' | grep inet | awk -F' ' '{print $2}' | grep [0-9])
+		echo -n "▀"
+		sleep 1
     fi
     done
     sleep 5
@@ -50,16 +54,17 @@ function waitForHost1
 config_network(){
         conf_dir="/etc/config"
         config="network"
-        uci set network.wlan=interface
-        uci set network.wlan.netmask='255.255.255.0'
-        uci set network.wlan.proto='dhcp'
-        uci commit $config
-		uci set firewall.cfg04dc81.network=lan
-		uci set firewall.cfg06dc81.network='wan wan6 wlan'
-		uci commit firewall
-        /etc/init.d/network restart
-        wifi up
+        uci -q set network.wlan=interface
+        uci -q set network.wlan.netmask='255.255.255.0'
+        uci -q set network.wlan.proto='dhcp'
+        uci commit network
+		uci -q set firewall.cfg04dc81.network=lan
+		uci -q set firewall.cfg06dc81.network='wan wan6 wlan'
+		wifi up
         waitForHost google.com
+		wifi up
+		/etc/init.d/firewall restart > /dev/null 2>&1
+		/etc/init.d/network restart > /dev/null 2>&1
 }
 config_wireless(){
         read -p "SSID: " SSID
@@ -68,57 +73,132 @@ config_wireless(){
         config="wireless"
         rm -rf $conf_dir/$config
         wifi config
-        sed -i 's/default_radio0/ap/' $conf_dir/$config
-        uci -q set wireless.ap.ssid='FDI-LEDE'
-        uci -q set wireless.ap.encryption=psk2
-        uci -q set wireless.ap.key='1234567890'
-        uci commit $config
+		sed -i 's/default_radio0/ap/' $conf_dir/$config
+		uci -q set wireless.ap.ssid='FDI-LEDE'
+		uci -q set wireless.ap.encryption='psk2'
+		uci -q set wireless.ap.key='1234567890'
+		uci commit wireless
         if [ "$SSID" != "" -o "$KEY" != "" ];then
-			uci -q set wireless.sta=wifi-iface
-			uci -q set wireless.sta.network='wlan'
-			uci -q set wireless.sta.encryption='psk2'
-			uci -q set wireless.sta.device='radio0'
-			uci -q set wireless.sta.mode='sta'
-			uci -q set wireless.sta.key="$KEY"
-			uci -q set wireless.sta.ssid="$SSID"
-			uci -q set wireless.radio0.disabled='0'
-			uci commit $config
+			uci set wireless.radio0.disabled='0'
+			uci set wireless.sta='wifi-iface'
+			uci set wireless.sta.network='wlan'
+			uci set wireless.sta.encryption='psk2'
+			uci set wireless.sta.device='radio0'
+			uci set wireless.sta.mode='sta'
+			uci set wireless.sta.key="$KEY"
+			uci set wireless.sta.ssid="$SSID"
+			uci commit wireless
 			config_network
 		fi
+}
+
+aap(){
+ping -w 10 google.com > /dev/null 2>&1
+if [ $? -eq 0 ];then
+	UNDERLINE=$(echo -e "\e[1;4mmode AP-STA")
+	echo "$UNDERLINE"
+	RED='\033[0;32m'
+	printf "${RED} Connected\n"
+	echo "mode AP+STA" > /root/wifi.log
+else
+	UNDERLINE=$(echo -e "\e[1;4mmode AP")
+	RED='\033[1;31m';
+	echo "$UNDERLINE"
+	printf "${RED}Not Connected\n"
+	uci -q del wireless.sta
+	uci commit wireless
+	wifi up
+	echo "mode AP" > /root/wifi.log
+fi
 }
 help(){
 echo "1.) fdi exroot"
 echo "2.) fdi update-script"
 echo "3.) fdi ap-sta wifi auto set"
 echo "4.) fdi install luci"
+echo "5.) fdi upgrade firmware"
+echo "6.) fdi aap how to enabled/disabled ?"
 }
+base_script(){
 help
 read -p "masukan pilihan [1-2] :" opt
 case $opt in
-        '1')
-                exroot
-        ;;
-        '2')
-                _host="http://felexindo.mooo.com"
-                ping -w 1 felexindo.mooo.com
-                if [ $? == 0 ];then
-                        cd /tmp && rm -rf fdi && wget --quiet $_host\:81/script/fdi
-                        cat /tmp/fdi > /usr/bin/fdi
-                else
-                        echo "maaf script belum relase atau periksa kembali koneksi anda"
-                fi
-        ;;
-        '3')
-				config_wireless $2 $3
-        ;;
-	'4')
-		echo "update.."
-		cd /tmp && opkg update > /dev/null 2>&1
-		echo "install luci..."
-		opkg install luci ca-bundle ca-certificates libustream-openssl  > /dev/null 2>&1
-		echo "install luci-theme-darkmatter"
-		wget https://apollo.open-resource.org/downloads/luci-theme-darkmatter_0.2-beta-2_all.ipk  > /dev/null 2>&1
-		opkg install luci-theme-darkmatter_0.2-beta-2_all.ipk  > /dev/null 2>&1
-		cd ~/
-	;;
+		'1')
+	    	exroot
+		;;
+		'2')
+			read -p "host aktif fdi lede [felexindo.mooo.com] :" host
+			read -p "port aktif fdi lede [81] :" port
+			if [ "$host" != "" ]&&[ "$port" != "" ];then
+				ping -w 1 $host > /dev/null 2>&1
+				if [ $? -eq 0 ];then
+					cd /tmp
+					rm -rf fdi
+					rm -rf html
+					wget http://$host:$port/script/fdi > /dev/null 2>&1
+					wget http://$host:$port/script/html > /dev/null 2>&1
+					if [ $? -eq 0 ];then
+						cp -rf /tmp/fdi /usr/bin/fdi
+						cp -rf /tmp/html /www/cgi-bin/html
+						chmod 0755 /usr/bin/fdi /www/cgi-bin/html
+						rm -rf fdi
+						rm -rf html
+						cd ~/
+					else
+						echo "maaf ada kesalahan dalam peng oprasian"
+						echo "silahkan ulangi"
+					fi
+				else
+					echo "maaf script belum relase atau periksa kembali koneksi anda"
+				fi
+			else
+				echo "host tidak aktif atau typo"
+			fi
+		;;
+		'3')
+			echo "list:"
+			echo "################"
+			iw wlan0 scan | grep SSID | awk -F':' '{print $2}' | sed -e 's/^\ *//'
+			echo "################"
+			config_wireless $2 $3
+		;;
+		'4')
+			read -p "host aktif fdi lede [felexindo.mooo.com] :" host
+			read -p "port aktif fdi lede [81] :" port
+			echo "ping source..."
+			ping -w 1 $host > /dev/null 2>&1
+			if [ $? -eq 0 ];then
+				echo "update.."
+				cd /tmp && opkg update > /dev/null 2>&1
+				echo "install luci..."
+				wget http://$host:$port/luci.tar.gz && tar -xzvf luci.tar.gz && cd luci && opkg install ca-bundle ca-certificates libustream-openssl ./*.ipk --force-depends --force-reinstall --force-overwrite --force-depends --force-reinstall --force-overwrite  > /dev/null 2>&1
+				echo "install luci-theme-darkmatter"
+				wget https://$host:$port/script/luci-theme-darkmatter_0.2-beta-2_all.ipk  > /dev/null 2>&1
+				opkg install luci-theme-darkmatter_0.2-beta-2_all.ipk  > /dev/null 2>&1
+				cd ~/
+				echo "fdi script updated.."
+			else
+				printf "\nconnection refused:\nplease repeart fdi wifi ap-sta\n"
+			fi
+		;;
+		'5')
+			FIND=$(find -L /tmp -name "*bin" | wc -l)
+			FILE=$(find -L /tmp -name "*bin" | tail -1)
+			if [ "$FIND" -ge 1 ];then
+				mtd -e firmware -r write $FILE firmware
+			else
+				echo "pastikan file firmware *.bin tersimpan di folder /tmp"
+			fi
+		;;
+		'6')
+			CRON="/etc/crontabs/root"
+			echo "echo '*/4 * * * * fdi aap' > $CRON # or you can use  crontab -e commands"
+			echo "/etc/init.d/cron start && /etc/init.d/cron enable"
+		;;
 esac
+}
+if [ "$1" == "aap" ];then
+	aap
+else
+	base_script
+fi
